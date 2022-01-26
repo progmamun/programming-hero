a6b65996ef79d3fe43b9b43ca1cc4190f581c1d4
progmamun <mamun333089@gmail.com>
2022-01-26 15:12:22 +0600

    quiz-solution

---
+++ milestone-4/quiz-mathematical-operation.js
@@ -0,0 +1,11 @@
+'use strict';
+// var num1 = '99.5';
+// var num2 = 15;
+
+// var num3 = parseInt(num1);
+
+// console.log(num3 + num2);
+
+var num1 = '99.5';
+var num2 = 15;
+console.log(num1 + num2);
