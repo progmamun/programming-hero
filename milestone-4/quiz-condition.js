6adbf7c2df47dd35bd56844a55213d3f1e7f040d
progmamun <mamun333089@gmail.com>
2022-01-27 11:36:22 +0600

    quiz-condition

---
+++ milestone-4/quiz-condition.js
@@ -0,0 +1,10 @@
+var cokePrice = 17;
+var isHot = true;
+
+if (cokePrice < 15) {
+  console.log('coke khabo');
+} else if (isHot == true) {
+  console.log('ami hot');
+} else {
+  console.log('pani khabo');
+}
