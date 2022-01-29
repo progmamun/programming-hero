397b52e9e94a402af5779c04ad8c9492e949727f
progmamun <mamun333089@gmail.com>
2022-01-29 14:15:12 +0600

    breack-continue

--- milestone-4/break-continue.js
+++ milestone-4/break-continue.js
@@ -3,6 +3,7 @@ let numbers = [55, 25, 23, 97, 101, 54, 65];
 for (let i = 0; i < numbers.length; i++) {
   let number = numbers[i];
   if (number > 90) {
+    // break;
     continue;
   }
   console.log(number);
