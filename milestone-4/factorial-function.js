1bbf41aeaa212af490a6562c2b70a3676682c51e
progmamun <mamun333089@gmail.com>
2022-01-30 14:47:54 +0600

    factorial-function-reverse

--- milestone-4/factorial-function.js
+++ milestone-4/factorial-function.js
@@ -10,3 +10,15 @@ function getFactorial(num) {
 
 let firstFactorial = getFactorial(9);
 console.log(`Factorial of 9 is ${firstFactorial}`);
+
+// for loop reverse
+function getFactorial2(number) {
+  let factorial = 1;
+  for (let i = number; i >= 1; i--) {
+    factorial = factorial * i;
+  }
+  return factorial;
+}
+
+const myFactorial2 = getFactorial2(6);
+console.log(myFactorial2);
