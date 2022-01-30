1de221aa4aac5c51ee51c826eb5d4105782ef4d5
progmamun <mamun333089@gmail.com>
2022-01-30 16:05:13 +0600

    quiz-function

---
+++ milestone-4/quiz-function-factorial.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// function inchToFeet(inch) {
+//   let feet = inch / 12;
+//   return feet;
+// }
+
+// let a = 6 * 5 * 4 * 3 * 2 * 1;
+// let a = 6 * (6 - 1);
+// let a = 6 * 5 * 3;
+// let a = 7 - 1;
+// if (a != 6) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
