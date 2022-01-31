fcac5526394ce678337ec812023731356303baa5
progmamun <mamun333089@gmail.com>
2022-01-31 11:57:09 +0600

    gradeCalculation

---
+++ milestone-4/homework/gradeCalculation.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let grade = 85;
+
+if (grade >= 80 && grade <= 100) {
+  console.log('You got A+');
+} else if (grade >= 70 && grade < 80) {
+  console.log('You got A');
+} else if (grade >= 60 && grade < 70) {
+  console.log('You got A');
+} else if (grade >= 50 && grade < 60) {
+  console.log('You got A');
+} else if (grade >= 40 && grade < 50) {
+  console.log('You got A');
+} else if (grade >= 33 && grade < 40) {
+  console.log('You got A');
+} else if (grade < 33) {
+  console.log('You are fail');
+} else {
+  console.log('Please Enter Valid Number:');
+}
