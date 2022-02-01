d92c2075812e42aa220cd7a939d40f383b5995c1
progmamun <mamun333089@gmail.com>
2022-02-01 22:32:37 +0600

    lowest-element.js

--- milestone-4/js-simple-problem/lowest-element.js
+++ milestone-4/js-simple-problem/lowest-element.js
@@ -13,17 +13,32 @@
 // }
 // console.log(minvalue);
 
-let array = [2, 3, -355, 25, 1];
+// let array = [2, 3, -355, 25, 1];
 
-function valueMin(arr) {
-  let minvalue = arr[0];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < minvalue) {
-      minvalue = arr[i];
+// function valueMin(arr) {
+//   let minvalue = arr[0];
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < minvalue) {
+//       minvalue = arr[i];
+//     }
+//   }
+//   return minvalue;
+// }
+
+// let p = valueMin(array);
+// console.log(p);
+function smallestElement(numbers) {
+  let min = numbers[0];
+
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element < min) {
+      min = element;
     }
   }
-  return minvalue;
+  return min;
 }
 
-let p = valueMin(array);
-console.log(p);
+const ages = [12, 54, 3, 35, -79, 35, 12];
+const small = smallestElement(ages);
+console.log('smallest', small);
