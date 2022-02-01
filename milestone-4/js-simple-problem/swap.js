1511bf580a1d376c64b73ac8d4124519f89a8769
progmamun <mamun333089@gmail.com>
2022-02-01 11:02:35 +0600

    swap-destructuring-array

---
+++ milestone-4/js-simple-problem/swap.js
@@ -0,0 +1,13 @@
+'use strict';
+let first = 7;
+let second = 5;
+
+// first method
+// let temp = first;
+// first = second;
+// second = temp;
+// console.log(first, second);
+
+// destructuring
+// [first, second] = [second, first];
+// console.log(first, second);
