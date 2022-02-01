bc17f6e198db266c33101922a2949db6b0244369
progmamun <mamun333089@gmail.com>
2022-02-01 14:55:32 +0600

    largest-element-find-out-array

---
+++ milestone-4/js-simple-problem/largest-element.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function largestElement(numbers) {
+  let largest = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    const element = numbers[i];
+    if (element > largest) {
+      largest = element;
+    }
+  }
+  return largest;
+}
+
+const ages = [12, 54, 3, 35, 79, 35, 12];
+const oldest = largestElement(ages);
+console.log('oldest', oldest);
