a793a8243be4cc259a84dd57cc638c358d887bd8
progmamun <mamun333089@gmail.com>
2022-02-01 22:04:22 +0600

    lowest-array-value-find-out

--- milestone-4/js-simple-problem/lowest-element.js
+++ milestone-4/js-simple-problem/lowest-element.js
@@ -4,11 +4,26 @@
 // const min = Math.min(...arr);
 // console.log(min);
 
-let array = [2, 3, -1, -9, 8];
-let minvalue = array[0];
-for (let i = 0; i < array.length; i++) {
-  if (array[i] < minvalue) {
-    minvalue = array[i];
+// let array = [2, 3, -1, -9, 8];
+// let minvalue = array[0];
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i] < minvalue) {
+//     minvalue = array[i];
+//   }
+// }
+// console.log(minvalue);
+
+let array = [2, 3, -355, 25, 1];
+
+function valueMin(arr) {
+  let minvalue = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < minvalue) {
+      minvalue = arr[i];
+    }
   }
+  return minvalue;
 }
-console.log(minvalue);
+
+var p = valueMin(array);
+console.log(p);
