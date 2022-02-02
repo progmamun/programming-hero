98539400b8d3e64fe351e8dd19ec43ef4ca1f61d
progmamun <mamun333089@gmail.com>
2022-02-02 16:00:42 +0600

    object-calculation

--- milestone-4/js-simple-problem/total-price.js
+++ milestone-4/js-simple-problem/total-price.js
@@ -11,4 +11,17 @@ let totalPrice = 0;
 for (const product of products) {
   totalPrice = totalPrice + product.price;
 }
-console.log(totalPrice);
+// console.log(totalPrice);
+
+const carts = [
+  { name: 'laptop', price: 50000, quantity: 1 },
+  { name: 'Shirt', price: 500, quantity: 4 },
+  { name: 'Phone', price: 15000, quantity: 1 },
+  { name: 'Watch', price: 3000, quantity: 1 },
+];
+let cartTotal = 0;
+for (const product of carts) {
+  const productTotal = product.price * product.quantity;
+  cartTotal = cartTotal + productTotal;
+}
+console.log(cartTotal);
