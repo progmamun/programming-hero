6b4882a3c1913ab673f81091ac5701eca9d40bf5
progmamun <mamun333089@gmail.com>
2022-02-02 15:23:46 +0600

    divisible-by

--- milestone-4/js-simple-problem/divisible-by.js
+++ milestone-4/js-simple-problem/divisible-by.js
@@ -1,11 +1,13 @@
 'use strict';
 
 for (let i = 1; i <= 50; i++) {
-  if (i % 3 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log('foobar');
+  } else if (i % 3 == 0) {
     console.log('foo');
   } else if (i % 5 == 0) {
     console.log('bar');
   } else {
-    console.log('i');
+    console.log(i);
   }
 }
