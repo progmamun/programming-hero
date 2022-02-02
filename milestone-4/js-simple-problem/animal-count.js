9bfdbdecf3caecd92178fff3db19405620ef8708
progmamun <mamun333089@gmail.com>
2022-02-02 16:33:12 +0600

    animal-count

--- milestone-4/js-simple-problem/animal-count.js
+++ milestone-4/js-simple-problem/animal-count.js
@@ -3,6 +3,7 @@
 function animalCount(miles) {
   const animalDensityFirst10Miles = 10;
   const animalDensitySecond50Miles = 50;
+  const animalDensityRestMiles = 100;
   if (miles <= 10) {
     const count = miles * animalDensityFirst10Miles;
     return count;
@@ -12,7 +13,14 @@ function animalCount(miles) {
     const secondDenseAnimals = restMiles * animalDensitySecond50Miles;
     const totalAnimals = first10 + secondDenseAnimals;
     return totalAnimals;
+  } else {
+    const first10 = 10 * animalDensityFirst10Miles;
+    const secondDenseAnimals = 10 * animalDensitySecond50Miles;
+    const restMiles = miles - 20;
+    const RestDenseAnimals = restMiles * animalDensityRestMiles;
+    const totalAnimals = first10 + secondDenseAnimals + RestDenseAnimals;
+    return totalAnimals;
   }
 }
-const animal = animalCount(19);
+const animal = animalCount(29);
 console.log(animal);
