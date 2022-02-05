8f46832cd9ca717df526e494c211fee54d1e2753
progmamun <mamun333089@gmail.com>
2022-02-05 11:16:30 +0600

    solve

--- milestone-4/js-simple-problem/module-23/assignment04.js
+++ milestone-4/js-simple-problem/module-23/assignment04.js
@@ -10,3 +10,18 @@ let vori = anaToVori(16);
 // console.log(vori);
 
 // Problem 02:
+function pandaCost(singaraQuantity, somusaQuantity, jilapiQuantity) {
+  const perSingaraPrice = 7;
+  const perSomusaPrice = 10;
+  const perJilapiPrice = 15;
+
+  const num1 = singaraQuantity * perSingaraPrice;
+  const num2 = somusaQuantity * perSomusaPrice;
+  const num3 = jilapiQuantity * perJilapiPrice;
+
+  const totalQuantity = num1 + num2 + num3;
+  return totalQuantity;
+}
+
+const order = pandaCost(1, 1, 1);
+// console.log(order);
