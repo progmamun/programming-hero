683c0c876e84b794ded674512fafd13874192f12
progmamun <mamun333089@gmail.com>
2022-02-05 12:41:04 +0600

    solve

--- milestone-4/js-simple-problem/module-23/assignment04.js
+++ milestone-4/js-simple-problem/module-23/assignment04.js
@@ -25,3 +25,32 @@ function pandaCost(singaraQuantity, somusaQuantity, jilapiQuantity) {
 
 const order = pandaCost(1, 1, 1);
 // console.log(order);
+
+// Problem 03:
+
+function picnicBudget(people) {
+  const people1 = 5000;
+  const people2 = 4000;
+  const people3 = 3000;
+  if (people <= 100) {
+    const count = people * people1;
+    return count;
+  } else if (people <= 200) {
+    const first100 = 100 * people1;
+    const restPeople = people - 100;
+    const secondPeople = restPeople * people2;
+    const totalPeople = first100 + secondPeople;
+    return totalPeople;
+  } else if (people <= 500) {
+    const first100 = 100 * people1;
+    const second200 = 100 * people2;
+    const restPeople = people - 200;
+    const restPeopleAll = restPeople * people3;
+    const totalAnimals = first100 + second200 + restPeopleAll;
+    return totalAnimals;
+  } else {
+    console.log('Please Enter Valid Number');
+  }
+}
+const totalPeopleCost = picnicBudget(501);
+console.log(totalPeopleCost);
