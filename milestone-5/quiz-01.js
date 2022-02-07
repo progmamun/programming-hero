c1f2ad4111aebd3c94112104ed59f3e3ff668f3d
progmamun <mamun333089@gmail.com>
2022-02-07 13:29:01 +0600

    quiz-

---
+++ milestone-5/quiz-01.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// document.getElementById('h1').style.fontSize = '30px';
+
+const ul = document.getElementById('ul');
+const li = document.createElement('li');
+li.innerHTML = `New item in the list`;
+ul.appendChild(li);
+
+// document.querySelectorAll('.blog h2');
