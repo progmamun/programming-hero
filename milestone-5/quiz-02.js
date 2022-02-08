e31f241391d4c6e0b19b7dfda370298cddaafe8f
progmamun <mamun333089@gmail.com>
2022-02-08 14:00:32 +0600

    quiz

---
+++ milestone-5/quiz-02.js
@@ -0,0 +1,11 @@
+{
+  /* <button onclick="myFunction()">Click me</button>  */
+}
+document.getElementById('order').addEventListener('click', function () {});
+
+{
+  /* <p class=”nav”>I am Hero Alam 1</p>
+<p class=”nav”>I am Hero Alam 2</p>
+
+var p = document.getElementsByClassName('nav')[0]; */
+}
