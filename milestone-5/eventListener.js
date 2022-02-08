e56bf22a4473c884ae997387b24a2a9f08448707
progmamun <mamun333089@gmail.com>
2022-02-08 08:34:46 +0600

    eventListener

--- milestone-5/eventListener.js
+++ milestone-5/eventListener.js
@@ -1,3 +1,19 @@
+// function
+function handleJustFunctionClick() {
+  const p = document.getElementById('click-method');
+  p.innerText = 'Set by using Function';
+}
+
+// direct
 document.getElementById('light-blue').addEventListener('click', function () {
   document.body.style.backgroundColor = 'lightblue';
+});
+
+// direct with input field
+document.getElementById('update-name').addEventListener('click', function () {
+  const nameField = document.getElementById('name-field');
+
+  const p = document.getElementById('click-method');
+  p.innerText = nameField.value;
+  nameField.value = '';
 });
