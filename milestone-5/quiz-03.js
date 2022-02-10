107d3277e07611211d0fe8c8103cf1ba27d5dcc0
progmamun <mamun333089@gmail.com>
2022-02-10 17:43:19 +0600

    quiz

---
+++ milestone-5/quiz-03.js
@@ -0,0 +1,16 @@
+// document.getElementById('balance').innerText = 1250;
+{
+  /* <p id="name">Programming Hero</p> */
+}
+
+const nameOfOrganization = document.getElementById('name').value;
+
+console.log(nameOfOrganization);
+
+{
+  /* <button id="button">Make Background Red</button>; */
+}
+
+document.getElementById('button').addEventListener('click', function () {
+  document.body.style.background = 'red';
+});
