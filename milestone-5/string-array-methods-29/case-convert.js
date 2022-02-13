adbc1c23f6f635e833f14a08422b4984f98a1854
progmamun <mamun333089@gmail.com>
2022-02-13 23:38:02 +0600

    case-convert

---
+++ milestone-5/string-array-methods-29/case-convert.js
@@ -0,0 +1,9 @@
+const anthem = 'amer sonAr bangla';
+const search = 'valo';
+
+const userInput = 'blaCkPink';
+const savedUserName = 'blackPink';
+
+if (userInput.toLowerCase() == savedUserName.toLocaleLowerCase()) {
+  console.log('user exists');
+}
