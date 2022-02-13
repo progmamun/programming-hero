b61a1fd22435ec97c3c3280769e4b7f43911f28d
progmamun <mamun333089@gmail.com>
2022-02-13 19:47:55 +0600

    quiz

---
+++ milestone-5/quiz-28.js
@@ -0,0 +1,24 @@
+'use strict';
+// let num = Math.pow(4, 0.5) + 'spiderman' + '987' + Math.sqrt(9);
+// console.log(num);
+
+// function generatePin() {
+//   return Math.floor(Math.random() * 90000) + 10000;
+// }
+// console.log(generatePin());
+
+// const product = 5;
+// const price = '7';
+// const subTotal = product * price;
+// const tax = subTotal / 10;
+// const total = subTotal + tax;
+
+// console.log(total);
+
+{
+  /* <button id="increment-btn">3</button> */
+}
+// const incrementButton = document.getElementById("increment-btn")
+// incrementButton.addEventListener("click", function () {
+// incrementButton.innerText = parseInt(incrementButton.innerText)+3;
+// })
