68475b6355d6cdb274fb9f4e9d704238ebbbf40d
progmamun <mamun333089@gmail.com>
2022-02-14 19:31:16 +0600

    quiz

---
+++ milestone-5/string-array-methods-29/quiz.js
@@ -0,0 +1,19 @@
+// const task = 'Practice Makes a person perfect';
+// const index = task.toLowerCase().indexOf('M');
+
+// const str1 = 'Bangladesh is a beautiful country.';
+// console.log(str1.endsWith('country'));
+
+// const str = 'I love coding more than eating.';
+// const check = str.slice(2, 6);
+// console.log(check);
+
+// const names = ['rafiq', 'jabbar', 'bakkar', 'habib', 'dalim'];
+// names.reverse();
+// console.log(names);
+
+// console.log(Date());
+
+const array = ['121', '12', '1', '112', '111'];
+
+console.log(array.sort());
