4e21b26a34a09cb7f3dd555b1fd568f4540a0fd4
progmamun <mamun333089@gmail.com>
2022-02-19 15:16:39 +0600

    quiz

---
+++ milestone-6/quiz-31.js
@@ -0,0 +1,13 @@
+// function getSum(a, b = 9) {
+//   return a + b;
+// }
+
+// console.log(getSum(2, 7));
+
+// const y = x => x * x;
+// const z = y(5);
+// console.log(z);
+
+const cars = ['toyota', 'honda', 'mercedes'];
+const newCars = [...cars, 'ferrari'];
+console.log(newCars);
--- milestone-6/readme.md
+++ milestone-6/readme.md
@@ -1,1 +1,6 @@
 # Milestone 6: Intermediate JavaScript, API
+
+- pop(): Remove an item from the end of an array.
+- push(): Add items to the end of an array.
+- shift(): Remove an item from the beginning of an array.
+- unshift(): Add items to the beginning of an array.
