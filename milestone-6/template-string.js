d06c7c280e37e7e7b5b10b1d4e85ab0c0c71bb03
progmamun <mamun333089@gmail.com>
2022-02-19 12:33:09 +0600

    template-string

---
+++ milestone-6/template-string.js
@@ -0,0 +1,9 @@
+const friends = ['abul', 'babul', 'kabul', 'sabul'];
+
+const first = 'Mamun';
+const last = 'Khan';
+
+const check = `His name is: ${first} ${last}. Have money ${
+  friends.length * 500
+}`;
+console.log(check);
