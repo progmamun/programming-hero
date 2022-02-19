c995b6f3acd611e79d619f94a636f0a7ef82b4c1
progmamun <mamun333089@gmail.com>
2022-02-19 14:03:40 +0600

    spread-operator

---
+++ milestone-6/spread-operator.js
@@ -0,0 +1,6 @@
+const numbers = [23, 65, 99, 21, 35];
+console.log(...numbers);
+
+const max = Math.max(23, 33, 343, 234, 1, 2, 33);
+const maxInArray = Math.max(...numbers);
+console.log(max, maxInArray);
