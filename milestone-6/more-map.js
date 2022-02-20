5afbcfb27d2a53d121f24d42a456bea6a27fc581
progmamun <mamun333089@gmail.com>
2022-02-20 12:21:34 +0600

    map-foreach

---
+++ milestone-6/more-map.js
@@ -0,0 +1,14 @@
+const friends = ['Tom Hanks', 'Tom Cruise', 'Tom Brady', 'Tom Solaiman'];
+const fLengths = friends.map(friend => friend.length);
+
+const products = [
+  { name: 'water bottle', price: 50, color: 'Yellow' },
+  { name: 'mobile phone', price: 15000, color: 'black' },
+  { name: 'smart watch', price: 1000, color: 'black' },
+  { name: 'Apple phone', price: 105000, color: 'black' },
+  { name: 'water glass', price: 10, color: 'white' },
+];
+const productNames = products.map(product => product.name);
+const productNames = products.map(product => product.price);
+// const productNames = products.map(product => console.log(product));
+// products.forEach(product => console.log(product));
