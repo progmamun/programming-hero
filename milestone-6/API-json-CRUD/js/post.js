eb4faf2d9f4256101d674160a13c38afa73139cc
progmamun <mamun333089@gmail.com>
2022-02-22 09:41:57 +0600

    post-fetch

--- milestone-6/API-json-CRUD/js/post.js
+++ milestone-6/API-json-CRUD/js/post.js
@@ -19,3 +19,18 @@ function displayPosts(posts) {
     postContainer.appendChild(div);
   }
 }
+
+// method : get | default
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then(response => response.json())
+  .then(json => console.log(json));
