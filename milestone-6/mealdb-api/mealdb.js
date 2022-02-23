e3bbbbf85fe98cda46176d77cfd61cff20684be3
progmamun <mamun333089@gmail.com>
2022-02-23 17:46:15 +0600

    mealdb

--- milestone-6/mealdb-api/mealdb.js
+++ milestone-6/mealdb-api/mealdb.js
@@ -8,5 +8,26 @@ const searchFood = () => {
   // console.log(url);
   fetch(url)
     .then(res => res.json())
-    .then(data => console.log());
+    .then(data => displaySearchResult(data.meals));
+};
+
+const displaySearchResult = meals => {
+  const searchResult = document.getElementById('search-result');
+  meals.forEach(meal => {
+    console.log(meal);
+    const div = document.createElement('div');
+    div.classList.add('col');
+    div.innerHTML = `
+    <div class="card h-100">
+  <img src="${meal.strMealThumb}" class="card-img-top img-fluid" alt="${
+      meal.strMeal
+    }">
+  <div class="card-body">
+    <h4 class="card-title">${meal.strMeal}</h4>
+    <p class="card-text">${meal.strInstructions.slice(0, 250)}</p>
+  </div>
+</div>
+    `;
+    searchResult.appendChild(div);
+  });
 };
