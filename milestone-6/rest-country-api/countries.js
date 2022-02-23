7cd919c171d1576c09afa0836eb6c4438951cb9c
progmamun <mamun333089@gmail.com>
2022-02-23 11:14:21 +0600

    countries-rest-api

--- milestone-6/rest-country-api/countries.js
+++ milestone-6/rest-country-api/countries.js
@@ -1,7 +1,7 @@
 'use strict';
 const loadCountries = () => {
-  // fetch('https://restcountries.com/v3.1/all')
-  fetch('https://restcountries.com/v2/all')
+  fetch('https://restcountries.com/v3.1/all')
+    // fetch('https://restcountries.com/v2/all')
     .then(res => res.json())
     .then(data => displayCountries(data));
 };
@@ -14,12 +14,35 @@ const displayCountries = counties => {
     console.log(country);
     const div = document.createElement('div');
     div.classList.add('country');
-    const h3 = document.createElement('h3');
-    h3.innerText = country.name;
-    div.appendChild(h3);
-    const p = document.createElement('p');
-    p.innerText = country.capital;
-    div.appendChild(p);
+    div.innerHTML = `
+      <h3>${country.name}</h3>
+      <p>${country.capital}</p>
+      <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+    `;
     countriesDiv.appendChild(div);
+    // const h3 = document.createElement('h3');
+    // h3.innerText = country.name;
+    // div.appendChild(h3);
+    // const p = document.createElement('p');
+    // p.innerText = country.capital;
+    // div.appendChild(p);
+    // countriesDiv.appendChild(div);
   });
+};
+
+const loadCountryByName = name => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]));
+};
+
+const displayCountryDetail = country => {
+  // console.log(country);
+  const countryDiv = document.getElementById('country-details');
+  countryDiv.innerHTML = `
+  <h4>${country.name}</h4>
+  <p>population: ${country.population}</p>
+  <img width="350px" height="350px" src="${country.flags.png}">
+  `;
 };
