2bb5b3b1c9f18c07c0667d76d5bbf6121dddee0c
progmamun <mamun333089@gmail.com>
2022-02-25 12:03:47 +0600

    quiz

---
+++ milestone-6/quiz-35.js
@@ -0,0 +1,36 @@
+// const x = undefined;
+
+// if (x) {
+//   console.log('Variable is truthy');
+// } else {
+//   console.log('Variable is Falsy');
+// }
+
+// let p = 'Javascript';
+// let q = p;
+// p = 'React';
+// console.log(q);
+
+// const isTrue = 'false';
+// if (!isTrue) {
+//   console.log('hello');
+// } else {
+//   console.log('world');
+// }
+
+// function sum(p, q) {
+//   p + q;
+// }
+// const result = sum(2, 3);
+// console.log(result);
+
+// if ('2' === 2) {
+//   console.log('Inside if');
+// } else {
+//   console.log('Inside else');
+// }
+
+// function work(x, y = 4) {
+//   return x + y;
+//  }
+//  console.log(work(32);
