6d148dd7487fde4f625c9987d01cae8739ad8986
progmamun <mamun333089@gmail.com>
2022-02-26 17:23:17 +0600

    bind-method

---
+++ milestone-6/object-concept-js/bind-method.js
@@ -0,0 +1,20 @@
+const kibria = {
+  id: 101,
+  money: 5000,
+  name: 'RJ kibria',
+  treatDey: function (expense) {
+    this.money = this.money - expense;
+    console.log('Here55', this);
+    return this.money;
+  },
+};
+
+const heroBalam = {
+  id: 102,
+  money: 6000,
+  name: 'Hero Balam',
+};
+// kibria.treatDey(100);
+
+const heroTreatDey = kibria.treatDey.bind(heroBalam);
+heroTreatDey(500);
