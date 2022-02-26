b1dd3d920e11022803aeffed1ec9d97b4638ad55
progmamun <mamun333089@gmail.com>
2022-02-26 19:49:35 +0600

    quiz

---
+++ milestone-6/object-concept-js/quiz-36.js
@@ -0,0 +1,12 @@
+// const nayok = { name: 'hero', age: '28', isMarried: false };
+// console.log(Object.entries(nayok));
+
+// const obj = { a: 1, b: 7, c: 3, length: 2 };
+// console.log(Object.keys(obj).length);
+
+// const obj1 = { module: 35, video: 2 };
+// const obj2 = { module: 35, video: 2 };
+// console.log(obj1 === obj2);
+
+const getGirlFriend = (name = 'chokina') => 'name';
+console.log(getGirlFriend());
