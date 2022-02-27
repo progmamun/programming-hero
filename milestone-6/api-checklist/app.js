3487e161c5214bb9059aa2b75f967821b4da6d85
progmamun <mamun333089@gmail.com>
2022-02-27 09:16:58 +0600

    update

---
+++ milestone-6/api-checklist/app.js
@@ -0,0 +1,51 @@
+function loadSingleUser() {
+  fetch()
+    .then(res => res.json())
+    .then(data => displaySingleUser(data.results[0]));
+}
+loadSingleUser();
+
+const displaySingleUser = user => {
+  console.log(user);
+};
+
+// meal db
+const toggleSpinner = displayStyle => {
+  document.getElementById('spinner').style.display = displayStyle;
+};
+const searchMeal = () => {
+  const searchText = document.getElementById('search-field').value;
+
+  // display spinner
+  toggleSpinner('block');
+
+  loadMeals(searchText);
+  document.getElementById('search-field').value = '';
+};
+
+const loadMeals = searchText => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displayMeals(data.meals));
+};
+const displayMeals = meals => {
+  const container = document.getElementById('meals');
+  container.textContent = '';
+  meals.forEach(meal => {
+    console.log(meal);
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <h1>${meal.strMeal}</h1>
+    <button onclick="loadMealDetail('${meal.strMeal}')">click me </button>
+    `;
+    container.appendChild(div);
+  });
+  toggleSpinner('none');
+};
+
+loadMeals('fish');
+
+const loadMealDetail = mealName => {
+  console.log(mealName);
+};
