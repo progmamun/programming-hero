5465bc361beb8ada0ee20c0f8fc35010df328017
progmamun <mamun333089@gmail.com>
2022-03-02 21:02:36 +0600

    closure

--- milestone-6/closure.js
+++ milestone-6/closure.js
@@ -12,6 +12,7 @@ let clock1 = stopWatch();
 // console.log(clock1());
 
 // ===== more closure =====///
+/*
 const bank = owner => {
   balance = 0;
   return amount => {
@@ -25,3 +26,26 @@ const MofijBank = bank('Mofij');
 console.log(MofijBank(100));
 console.log(MofijBank(300));
 console.log(MofijBank(500));
+*/
+
+const bank = owner => {
+  balance = 0;
+  return {
+    deposit: amount => {
+      // balance = balance + amount;
+      balance += amount;
+      return balance;
+    },
+    withdraw: amount => {
+      // balance = balance - amount;
+      balance -= amount;
+      return balance;
+    },
+  };
+};
+const MofijBank = bank('Mofij');
+console.log(MofijBank.deposit(100));
+console.log(MofijBank.deposit(300));
+console.log(MofijBank.deposit(500));
+console.log(MofijBank.withdraw(50));
+console.log(MofijBank.withdraw(100));
