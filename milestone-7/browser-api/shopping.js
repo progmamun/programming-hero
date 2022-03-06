22838b1f3b6fcf7e361c9bda0ad5037982cff69d
progmamun <mamun333089@gmail.com>
2022-03-06 20:27:42 +0600

    dynamically-set-local-session-storage

--- milestone-7/browser-api/shopping.js
+++ milestone-7/browser-api/shopping.js
@@ -1,4 +1,12 @@
 'use strict';
+
+const displayLocalStorageCart = () => {
+  const cart = getCart();
+  for (const name in cart) {
+    displayProduct(name);
+  }
+};
+
 const addItem = () => {
   const nameField = document.getElementById('product-name');
   const name = nameField.value;
@@ -32,8 +40,19 @@ const getCart = () => {
 
 const addProductToCart = name => {
   const cart = getCart();
-  cart[name] = 1;
+  // cart[name] = 1;
+  if (cart[name]) {
+    cart[name] = cart[name] + 1;
+  } else {
+    cart[name] = 1;
+  }
   // console.log(cart);
   const cartStringified = JSON.stringify(cart);
   localStorage.setItem('cart', cartStringified);
 };
+
+const placeOrder = () => {
+  document.getElementById('products').textContent = '';
+  localStorage.removeItem('cart');
+};
+displayLocalStorageCart();
