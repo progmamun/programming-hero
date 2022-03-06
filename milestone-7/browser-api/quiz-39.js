87373e4a667330d95913ba9e7fd079e4717e029b
progmamun <mamun333089@gmail.com>
2022-03-06 21:25:31 +0600

    quiz

---
+++ milestone-7/browser-api/quiz-39.js
@@ -0,0 +1,16 @@
+// Browser Object Model (BOM)
+
+// document.getElementById('go-back').addEventListener('click', () => {
+//   history.back();
+// });
+
+// document.cookie will return all cookies in one string
+
+// let path x = location.pathname;
+
+/*
+{ <a id="anchor" href="https://web.programming-hero.com/dashboard">Visit</a> }
+const url = document.getElementById("anchor");
+const path = url.pathname;
+console.log(path);
+*/
