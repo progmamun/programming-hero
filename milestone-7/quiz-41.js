ae2f32d5ebc0b508bdd62b2ef58e1dd6a20c65e9
progmamun <mamun333089@gmail.com>
2022-03-07 20:11:36 +0600

    quiz

---
+++ milestone-7/quiz-41.js
@@ -0,0 +1,32 @@
+'use strict';
+// const addBtn = document.getElementById('add-btn');
+// addBtn.className = 'btn-primary';
+// addBtn.classList.add('btn-primary');
+// addBtn.setAttribute('class', 'btn-primary');
+
+// const marks = 60;
+// const resubmit = -5;
+// console.log(!(marks > 0 || resubmit > 0));
+
+/* How will you find out the numbers of properties present in an object person?
+
+const person = { name: 'hero', id: 101, address: 'BD', postalCode: 40321 };
+console.log(Object.keys(person).length);
+*/
+
+// const animals = ['cat', 'dog', 'rat'];
+// console.log(animals.includes('Cat'));
+
+// array = [2, 3, 4, 5, 2];
+// const unique = array => array.indexOf('21');
+
+// const add = (para1, para2) => para2 + para1;
+// console.log(add('2', 3));
+
+// const b = 6,
+//   b = 6;
+// console.log(b);
+
+// What is a regular expression: A regular expression is a sequence of characters that specifies a search pattern in text.
+const colors = { mango: 'green', grapes: 'black', organe: 'yellow' };
+console.log(colors[grapes]);
