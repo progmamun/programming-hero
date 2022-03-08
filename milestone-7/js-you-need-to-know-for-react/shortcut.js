cec30e9447f9f64040ecacf62ddbfff21fbfd8f8
progmamun <mamun333089@gmail.com>
2022-03-08 12:45:09 +0600

    toggle-boolean

--- milestone-7/js-you-need-to-know-for-react/shortcut.js
+++ milestone-7/js-you-need-to-know-for-react/shortcut.js
@@ -22,3 +22,16 @@ console.log(numStr);
 const input = '555';
 const inputNum = +input;
 console.log(inputNum);
+
+///
+let isActive = true;
+const showUser = () => console.log('display user');
+const hideUser = () => console.log('hide user');
+isActive ? showUser() : hideUser();
+// use && if the left side is true then right side will be executed
+isActive && showUser();
+// use || if the left side is false then right side will be executed
+isActive || hideUser();
+
+// toggle boolean
+isActive = !isActive;
