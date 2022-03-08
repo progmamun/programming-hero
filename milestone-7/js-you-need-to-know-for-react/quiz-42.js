a517fabf979451bf2972e23daaf08c84fe183617
progmamun <mamun333089@gmail.com>
2022-03-08 15:30:41 +0600

    quiz

---
+++ milestone-7/js-you-need-to-know-for-react/quiz-42.js
@@ -0,0 +1,18 @@
+// const name = 'Hero';
+// const age = 34;
+// const person = { name, age };
+// console.log(person);
+
+// const person = { name: 'Babe' };
+// console.log(person.name);
+// console.log(person['name']);
+
+// const adventurer = { name: 'Alice', cat: { name: 'Lucy' } };
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+
+// let person = null;
+// console.log(person ? person : 'person is null');
+
+const obj = { a: 1 };
+console.log(Object.keys(obj).length === 0);
