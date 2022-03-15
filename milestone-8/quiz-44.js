a0efd6daa090ab55bb9226bb41b7156598b60260
progmamun <mamun333089@gmail.com>
2022-03-15 21:47:42 +0600

    quiz

---
+++ milestone-8/quiz-44.js
@@ -0,0 +1,9 @@
+// const nayok = {
+//   name: 'Salman Shah',
+//   birthPlace: 'sylhet',
+//   firstSerial: 'Pathor Somoy ',
+// };
+
+// let ul = `<li>Programming<li>`;
+// ul += `<li>Hero<li>`;
+// console.log(ul);
