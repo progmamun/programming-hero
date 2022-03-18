bbf7b5b75b0647a8b38c87c6753c2920cd8d69bf
progmamun <mamun333089@gmail.com>
2022-03-18 10:57:11 +0600

    distruction

--- milestone-8/rest-countries-reactjs/src/components/Country/Country.js
+++ milestone-8/rest-countries-reactjs/src/components/Country/Country.js
@@ -3,13 +3,13 @@ import './Country.css';
 
 const Country = props => {
   // console.log(props.country.area);
-  const { area } = props.country;
+  const { area, region, population, name } = props.country;
   return (
     <div className="country">
-      <h2>Country Name:{props.country.name.common}</h2>
-      <h4>population: {props.country.population}</h4>
+      <h2>Country Name:{name.common}</h2>
+      <h4>population: {population}</h4>
       <p>
-        <small>Region: {props.country.region}</small>
+        <small>Region: {region}</small>
       </p>
       <p>Area: {area}</p>
       <img src={props.country.flags.png} alt="" />
