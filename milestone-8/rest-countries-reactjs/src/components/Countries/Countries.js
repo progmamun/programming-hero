11dcdb0a7f46163692da3c09c8d9367f01b3ab85
progmamun <mamun333089@gmail.com>
2022-03-18 10:55:13 +0600

    multiple-data-pass

--- milestone-8/rest-countries-reactjs/src/components/Countries/Countries.js
+++ milestone-8/rest-countries-reactjs/src/components/Countries/Countries.js
@@ -14,9 +14,7 @@ const Countries = () => {
       <h1>Hello from Countries.{countries.length}</h1>
       {countries.map(country => (
         <Country
-          name={country.name.common}
-          population={country.population}
-          area={country.area}
+          country={country}
         ></Country>
       ))}
     </div>
--- milestone-8/rest-countries-reactjs/src/components/Country/Country.js
+++ milestone-8/rest-countries-reactjs/src/components/Country/Country.js
@@ -2,11 +2,17 @@ import React from 'react';
 import './Country.css';
 
 const Country = props => {
+  // console.log(props.country.area);
+  const { area } = props.country;
   return (
     <div className="country">
-      <h2>Country Name:{props.name}</h2>
-      <h4>population: {props.population}</h4>
-      <p>Area: {props.area}</p>
+      <h2>Country Name:{props.country.name.common}</h2>
+      <h4>population: {props.country.population}</h4>
+      <p>
+        <small>Region: {props.country.region}</small>
+      </p>
+      <p>Area: {area}</p>
+      <img src={props.country.flags.png} alt="" />
     </div>
   );
 };
