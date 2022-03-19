79f5d905a031709a51012a9531627fb9aac4d9ed
progmamun <mamun333089@gmail.com>
2022-03-19 11:45:04 +0600

    useEffect-display-data

--- milestone-8/react-core-concept-recap/src/App.js
+++ milestone-8/react-core-concept-recap/src/App.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoadPosts></LoadPosts>
       <District name="Rajshahi" special="Mango"></District>
       <District name="Dhaka" special="capital"></District>
       <District name="Chittagong" special="Bay of Bengal"></District>
@@ -16,6 +17,40 @@ const districtStyle = {
   borderRadius: '20px',
   padding: '20px',
 };
+
+function LoadPosts(data) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Post: {posts.length}</h1>
+      {posts.map(post => (
+        <Post title={post.title} body={post.body} id={post.id}></Post>
+      ))}
+    </div>
+  );
+}
+
+function Post(props) {
+  return (
+    <div
+      style={{
+        backgroundColor: 'gold',
+        margin: '20px',
+        border: '4px solid salmon',
+      }}
+    >
+      <h1>ID: {props.id}</h1>
+      <h2>Title: {props.title}</h2>
+      <p>Body: {props.body}</p>
+    </div>
+  );
+}
 
 function District(props) {
   const [power, setPower] = useState(1);
