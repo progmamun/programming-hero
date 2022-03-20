b311f09943a8f45af9128246a6394c04d9267940
progmamun <mamun333089@gmail.com>
2022-03-21 00:59:09 +0600

    import

---
+++ milestone-8/react-storage-and-handler/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Cosmetics from './components/Cosmetics/Cosmetics';
+
+function App() {
+  return (
+    <div className="App">
+      <Cosmetics></Cosmetics>
+    </div>
+  );
+}
+
+export default App;
---
+++ milestone-8/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+  return (
+    <div>
+      <h2>This is cosmetics components</h2>
+    </div>
+  );
+};
+
+export default Cosmetics;
