3623524d0f7bf41a4fd8a9c904e3a09d3152f80c
progmamun <mamun333089@gmail.com>
2022-03-21 11:51:28 +0600

    multiple-import

--- milestone-8/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ milestone-8/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -6,7 +6,7 @@ const Cosmetics = () => {
   const first = 55;
   const second = 66;
   // const total = add(first, second);
-  const total = addssss(first, second);
+  const total = add(first, second);
   return (
     <div>
       <h2>This is cosmetics components</h2>
