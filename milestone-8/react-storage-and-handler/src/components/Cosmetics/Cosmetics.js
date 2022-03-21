4173053cc4a5afee61840e404b837334bef9a806
progmamun <mamun333089@gmail.com>
2022-03-21 11:50:57 +0600

    multiple-import

--- milestone-8/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
+++ milestone-8/react-storage-and-handler/src/components/Cosmetics/Cosmetics.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { add } from '../utilities/calculate';
+// import add from '../utilities/calculate';
 
 const Cosmetics = () => {
+  const first = 55;
+  const second = 66;
+  // const total = add(first, second);
+  const total = addssss(first, second);
   return (
     <div>
       <h2>This is cosmetics components</h2>
+      <p>Total: {total}</p>
     </div>
   );
 };
---
+++ milestone-8/react-storage-and-handler/src/components/utilities/calculate.js
@@ -0,0 +1,10 @@
+const add = (first, second) => {
+  return first + second;
+};
+
+const multiply = (first, second) => {
+  return first * second;
+};
+export { add, multiply };
+
+// export default add;
