a119808ab7a1b7a7ca25192bc0158732739464ea
progmamun <mamun333089@gmail.com>
2022-04-20 14:26:26 +0600

    dynamic-api

--- milestone-11/smarty-node/index.js
+++ milestone-11/smarty-node/index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Hello from my personal smarty app');
 });
 
+const users = [
+  { id: 1, name: 'Mamun', email: 'mamun@gmail.com' },
+  { id: 2, name: 'Mamun', email: 'mamun@gmail.com' },
+  { id: 3, name: 'Mamun', email: 'mamun@gmail.com' },
+  { id: 4, name: 'Mamun', email: 'mamun@gmail.com' },
+];
+
+app.get('/users', (req, res) => {
+  res.send(users);
+});
+
+app.get('/user/:id', (req, res) => {
+  console.log(req.params);
+  const id = parseInt(req.params.id);
+  const user = users.find(u => u.id === id);
+  res.send(user);
+});
+
 app.listen(port, () => {
   console.log('Listening to port', port);
 });
