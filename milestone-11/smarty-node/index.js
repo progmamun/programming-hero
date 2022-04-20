dd7600faa8bb0644af1dbaade17b6c5025a09712
progmamun <mamun333089@gmail.com>
2022-04-20 18:01:06 +0600

    explore-query-parameter

--- milestone-11/smarty-node/index.js
+++ milestone-11/smarty-node/index.js
@@ -39,7 +39,15 @@ const users = [
 ];
 
 app.get('/users', (req, res) => {
-  res.send(users);
+  if (req.query.name) {
+    const search = req.query.name.toLocaleLowerCase();
+    const matched = users.filter(user =>
+      user.name.toLocaleLowerCase().includes(search)
+    );
+    res.send(matched);
+  } else {
+    res.send(users);
+  }
 });
 
 app.get('/user/:id', (req, res) => {
@@ -51,7 +59,10 @@ app.get('/user/:id', (req, res) => {
 
 app.post('/user', (req, res) => {
   console.log('request', req.body);
-  res.send('post method success');
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
 });
 
 app.listen(port, () => {
