8b37cbff9f36d7310a7d6ac527ed4c9242b5a725
progmamun <mamun333089@gmail.com>
2023-01-17 13:51:18 +0600

    part-3-remove-inside-reducer

--- ACC-Redux/module-3/src/pages/Cart.js
+++ ACC-Redux/module-3/src/pages/Cart.js
@@ -6,9 +6,11 @@ const Cart = () => {
   const cart = useSelector((state) => state.cart);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      {cart.map((product) => (
-        <ProductCard product={product} />
-      ))}
+      {cart
+        .sort((a, b) => a._id - b._id)
+        .map((product) => (
+          <ProductCard product={product} />
+        ))}
     </div>
   );
 };
--- ACC-Redux/module-3/src/redux/reducers/productReducer.js
+++ ACC-Redux/module-3/src/redux/reducers/productReducer.js
@@ -28,6 +28,17 @@ const productReducer = (state = initialState, action) => {
         cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     case REMOVE_TO_CART:
+      if (selectedProduct.quantity > 1) {
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
+
+        selectedProduct.quantity = selectedProduct.quantity - 1;
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
       return {
         ...state,
         cart: state.cart.filter(
