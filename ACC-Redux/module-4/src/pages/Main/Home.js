0c4dd780d4a69da2731b7764c01674d2802f7706
progmamun <mamun333089@gmail.com>
2023-01-18 12:31:38 +0600

    handling-ui-data-according-to-filter

--- ACC-Redux/module-4/src/pages/Main/Home.js
+++ ACC-Redux/module-4/src/pages/Main/Home.js
@@ -17,6 +17,31 @@ const Home = () => {
 
   const activeClass = "text-white  bg-indigo-500 border-white";
 
+  let content;
+
+  if (products.length) {
+    content = products.map((product) => (
+      <ProductCard key={product.model} product={product} />
+    ));
+  }
+
+  if (products.length && (stock || brands.length)) {
+    content = products
+      .filter((product) => {
+        if (stock) {
+          return product.status === true;
+        }
+        return product;
+      })
+      .filter((product) => {
+        if (brands.length) {
+          return brands.includes(product.brand);
+        }
+        return product;
+      })
+      .map((product) => <ProductCard key={product.model} product={product} />);
+  }
+
   return (
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
@@ -46,9 +71,7 @@ const Home = () => {
         </button>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
-        {products.map((product) => (
-          <ProductCard key={product.model} product={product} />
-        ))}
+        {content}
       </div>
     </div>
   );
