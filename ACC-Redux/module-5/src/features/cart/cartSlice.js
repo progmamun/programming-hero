890188038eae4f3a2f92fb17234fc85f650501f2
progmamun <mamun333089@gmail.com>
2023-01-27 21:56:08 +0600

    handing-add-to-cart-and-quantity

--- ACC-Redux/module-5/src/features/cart/cartSlice.js
+++ ACC-Redux/module-5/src/features/cart/cartSlice.js
@@ -7,7 +7,26 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const selectedProduct = state.cart.find(
+        (product) => product._id === action.payload._id
+      );
+
+      if (!selectedProduct) {
+        const product = { ...action.payload, quantity: 1 };
+        state.cart.push(product);
+      } else {
+        selectedProduct.quantity += 1;
+
+        state.cart
+          .filter((product) => product._id !== selectedProduct._id)
+          .push(selectedProduct);
+      }
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
