90e0ee0f98bc0870a7bb90a5b2396db1547daf7c
progmamun <mamun333089@gmail.com>
2023-02-05 09:06:28 +0600

    code-splitting-inc

--- ACC-Redux/module-7(final-project-part-1)/src/app/store.js
+++ ACC-Redux/module-7(final-project-part-1)/src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../feature/api/apiSlice";
 import authSlice from "../feature/auth/authSlice";
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
--- ACC-Redux/module-7(final-project-part-1)/src/feature/api/apiSlice.js
+++ ACC-Redux/module-7(final-project-part-1)/src/feature/api/apiSlice.js
@@ -2,6 +2,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_DEV_URL,
+  }),
   endpoints: (builder) => ({}),
 });
+
+export default apiSlice;
