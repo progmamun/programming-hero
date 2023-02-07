ffb0872562903e203ad899ab292398d44902f63a
progmamun <mamun333089@gmail.com>
2023-02-07 13:19:19 +0600

    connection-user-to-database

--- ACC-Redux/module-8(part-2)/src/feature/auth/authSlice.js
+++ ACC-Redux/module-8(part-2)/src/feature/auth/authSlice.js
@@ -27,7 +27,11 @@ export const getUser = createAsyncThunk("auth/getUser", async (email) => {
   const res = await fetch(`${process.env.REACT_APP_DEV_URL}/user/${email}`);
   const data = await res.json();
 
-  return data.data;
+  if (data.status) {
+    return data;
+  }
+
+  return email;
 });
 
 export const loginUser = createAsyncThunk(
@@ -121,7 +125,11 @@ const authSlice = createSlice({
       })
       .addCase(getUser.fulfilled, (state, { payload }) => {
         state.isLoading = false;
-        state.user = payload;
+        if (payload.status) {
+          state.user = payload.data;
+        } else {
+          state.user.email = payload;
+        }
         state.isError = false;
         state.error = "";
       })
--- ACC-Redux/module-8(part-2)/src/utils/PrivateRoute.js
+++ ACC-Redux/module-8(part-2)/src/utils/PrivateRoute.js
@@ -5,7 +5,10 @@ import Loading from "../components/reusable/Loading";
 
 const PrivateRoute = ({ children }) => {
   const { pathname } = useLocation();
-  const { isLoading, email } = useSelector((state) => state.auth);
+  const {
+    isLoading,
+    user: { email },
+  } = useSelector((state) => state.auth);
 
   if (isLoading) {
     return <Loading />;
