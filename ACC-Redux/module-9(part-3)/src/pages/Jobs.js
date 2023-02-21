492c62f53053ca4cc81f1be1fc4bd76cbc8a3854
progmamun <mamun333089@gmail.com>
2023-02-21 18:23:16 +0600

    fixed-job-page

--- ACC-Redux/module-9(part-3)/src/pages/Jobs.js
+++ ACC-Redux/module-9(part-3)/src/pages/Jobs.js
@@ -1,31 +1,22 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import JobCard from "../components/reusable/JobCard";
-import { useGetJobsQuery } from "../feature/job/jobApi";
+import { useGetJobsQuery } from "../feature/job/jobAPI";
 
 const Jobs = () => {
   const navigate = useNavigate();
   const { data, isLoading, isError } = useGetJobsQuery();
+  console.log(data);
   return (
     <div className="pt-14">
-      <h1>This is job page</h1>
-      <div>
-        {data?.data?.map(({ position, companyName, _id }) => (
-          <div>
-            <h1>{position}</h1>
-            <p>{companyName}</p>
-            <button onClick={() => navigate(`/job-details/${_id}`)}>
-              Details
-            </button>
-          </div>
-        ))}
-      </div>
-      {/* <div className="bg-primary/10 p-5 rounded-2xl">
+      <div className="bg-primary/10 p-5 rounded-2xl">
         <h1 className="font-semibold text-xl">Find Jobs</h1>
       </div>
       <div className="grid grid-cols-2 gap-5 mt-5">
-        <JobCard />
-      </div> */}
+        {data?.data?.map((jobData) => (
+          <JobCard key={jobData._id} jobData={jobData} />
+        ))}
+      </div>
     </div>
   );
 };
